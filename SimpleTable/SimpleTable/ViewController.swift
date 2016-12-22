747ba2080ea6515157525205973433635692d711
Erik Batista <erikbatista42@gmail.com>
2016-12-21 20:10:45 -0500

    added data source and delegate on tableview

--- SimpleTable/SimpleTable/ViewController.swift
+++ SimpleTable/SimpleTable/ViewController.swift
@@ -21,6 +21,31 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
-    
+    //
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return restaurantNames.count
+        
+    }
+    //
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cellIdentifier = "Cell"
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+        return cell
+        
+    }
+
 }
+
+
+
+
+
+
+
 
