9a7328ceaba5ab8239aa5f31eea88b4692122fad
Erik Batista <erikbatista42@gmail.com>
2016-12-26 12:57:15 -0500

    new cell view controller + connected outlets

---
+++ FoodPin/FoodPin/RestaurantTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantTableViewCell.swift
+//  FoodPin
+//
+//  Created by Erik Batista on 12/26/16.
+//  Copyright © 2016 swiftlang.eu. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantTableViewCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    @IBOutlet var thumbnailImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        
+    }
+
+}
