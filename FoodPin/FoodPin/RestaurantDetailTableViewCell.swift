7d6aad970b3a13f6b100b26c6daa871c82ab4744
Erik Batista <erikbatista42@gmail.com>
2016-12-31 09:54:55 -0500

    completed table view cell[detail table view]

---
+++ FoodPin/FoodPin/RestaurantDetailTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RestaurantDetailTableViewCell.swift
+//  FoodPin
+//
+//  Created by Erik Batista on 12/31/16.
+//  Copyright © 2016 swiftlang.eu. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailTableViewCell: UITableViewCell {
+    
+    @IBOutlet var fieldLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
