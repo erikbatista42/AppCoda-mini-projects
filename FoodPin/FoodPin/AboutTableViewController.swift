385f17dcb97d980a0686008a2fddfa2b5c1768e5
Erik Batista <erikbatista42@gmail.com>
2017-01-15 23:31:40 -0500

    all buttons on about button now functions

--- FoodPin/FoodPin/AboutTableViewController.swift
+++ FoodPin/FoodPin/AboutTableViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SafariServices
 
 class AboutTableViewController: UITableViewController {
     
@@ -69,6 +70,13 @@ class AboutTableViewController: UITableViewController {
                 }
             } else if indexPath.row == 1 {
                 performSegue(withIdentifier: "showWebView", sender: self)
+            }
+            
+            //Follow us section
+        case 1:
+            if let url = URL(string: links[indexPath.row]) {
+                let safariController = SFSafariViewController(url: url)
+                present(safariController, animated: true, completion: nil)
             }
             
         default:
